b9759f4b09f22fc2c2af1d28fe56392b12e8a539
jugettje <39992546+jugettje@users.noreply.github.com>
2018-11-27 16:11:20 +0100

    Add files via upload

---
+++ Timer/JR_Timer.cpp
@@ -0,0 +1,10 @@
+﻿#include "C:\Users\jurgen\Desktop\MicroControllers\Atmega_Lib_JR-master\Timer\JR_Timer.h"
+#include <avr/io.h>
+
+using namespace std;
+using namespace JR;
+
+_Timer::turnLEDOn(void)
+{
+	LED_ON;
+}
---
+++ Timer/JR_Timer.h
@@ -0,0 +1,14 @@
+#ifndef JR_TIMER_H
+#define JR_TIMER_H
+	
+namespace JR
+{
+	class _Timer
+	{
+	public:
+		void turnLEDOn(void);
+	};
+}
+
+
+#endif
